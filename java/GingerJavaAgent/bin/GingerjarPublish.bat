1accec320077058ce145aa8013493f6846226d42
Mohd Ambar Khan <MohdAmbar.Khan@amdocs.com>
2018-08-30 20:21:34 +0530

    Settiing Up Project

---
+++ java/GingerJavaAgent/bin/GingerjarPublish.bat
@@ -0,0 +1,3 @@
+
+"c:\Program Files (x86)\java\jdk1.6.0_29\bin\jar.exe"  cvfm D:\GingerSources\Devs\GingerNextVer_Dev\Ginger\ExternalAgents\GingerAgent.jar D:\GingerSources\Devs\GingerNextVer_Dev\GingerCore\Drivers\JavaDriverLib\GingerJavaAgent\maNIFEST.MF . .
+C:\ant\bin\ant -buildfile D:\GingerSources\Devs\GingerNextVer_Dev\Ginger\GingerAgentStarter.xml
